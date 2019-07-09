2738fffc5e1724b16ac812b47d816f59ecdd8d31
Thanh Tran <ttran@tims-macbook-pro.qsr.com.au>
2019-07-09 13:15:03 +1000

    initial commit

--- test/ViewController.swift
+++ test/ViewController.swift
@@ -10,8 +10,20 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    struct Resolution {
+        var width = 0
+        var height = 0
+    }
+    class VideoMode {
+        var resolution = Resolution()
+        var name:String?
+        var frameRate = 0.0
+        var interlaced = false
+    }
+    
     override func viewDidLoad() {
-        super.viewDidLoad()
+        let somevideo = VideoMode()
+        print("video_res_width:", somevideo.resolution.width)
         // Do any additional setup after loading the view, typically from a nib.
     }
 
